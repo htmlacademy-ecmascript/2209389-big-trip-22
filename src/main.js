3b1d94442d65aa84a9589ab4bd91a75678e717be
Никита Погорелов <nikipog@mail.ru>
2024-01-31 16:41:01 +0300

    Добавит модель для фильтров

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import PointModel from './model/point-model.js';
 import TripPresenter from './presenter/trip-presenter.js';
+import FilterModel from './model/filter-model.js';
 
 
 const siteMainElement = document.querySelector('.page-main');
@@ -10,6 +11,8 @@ const filterElement = document.querySelector('.trip-controls__filters');
 
 const pointModel = new PointModel();
 pointModel.init();
+
+const filterModel = new FilterModel();
 
 
 const tripPresenter = new TripPresenter({
