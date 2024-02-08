b281b417b5e43cd966ab2ee281927540259eebb3
Никита Погорелов <nikipog@mail.ru>
2024-02-08 18:34:47 +0300

    Защита: исправлены базовые критерии

--- src/view/point-edit-view.js
+++ src/view/point-edit-view.js
@@ -98,20 +98,20 @@ const createPointEditTemplate = (point, destinations, offers) => {
       </section>`
       : ''}
 
-      ${pointDestination ? (
+      ${pointDestination.length ?
       `<section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
         <p class="event__destination-description">${description}</p>
         ${pictures.length ? (
-        `
+      `
           <div class="event__photos-container">
           <div class="event__photos-tape">
           ${pictures.map((pic) => `<img class="event__photo" src="${pic.src}" alt="${pic.description}">`)}
           </div>
         </div>`
-      ) : '' }
-             </section> `
     ) : '' }
+             </section> `
+      : '' }
     </section>
     </form>
     </li>`
